f5bc156f648e1481dc22338224ccb5414fdf6bdd
rjoreis <rjoreis92@gmail.com>
2024-02-12 23:52:38 +0000

    Add swiper carousel

--- js/script.js
+++ js/script.js
@@ -221,6 +221,56 @@ function displayBackgroundImage(type, backgroundPath) {
 }
 
 
+// Display Slider Movies
+async function displaySlider() {
+    const { results } = await fetchAPIData('movie/now_playing');
+    console.log(results);
+    results.forEach((movie) => {
+        const div = document.createElement('div');
+        div.classList.add('swiper-slide');
+
+        div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+                <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4>
+        `;
+        document.querySelector('.swiper-wrapper').appendChild(div);
+
+        initSwiper();
+    })
+}
+
+function initSwiper() {
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        freeMode: true,
+        loop: true,
+        autoplay: {
+            delay: 4000,
+        },
+        breakpoints: {
+            500: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            700: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            1200: {
+                slidesPerView: 4,
+                spaceBetween: 30
+            },
+        }
+    });
+    
+}
+
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
     // Register key at https://www.themoviedb.org/settings/api
@@ -271,6 +321,7 @@ function init() {
     switch(global.currentPage) {
         case '/':
         case '/index.html':
+            displaySlider();
             displayPopularMovies();
             break;
         case '/shows.html':
